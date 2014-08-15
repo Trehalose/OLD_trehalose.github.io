3654444503d39129915b16380b71597eac36d37c
Trehalose <crisisabort@yahoo.com>
2014-08-15 12:42:30 -0700

    test
    see if it works now, started testing on different source area.

--- ARExp/index.js
+++ ARExp/index.js
@@ -1,131 +1,45 @@
-﻿
-//RUN WHEN THE PAGE CONTENT LOADS
-window.addEventListener('DOMContentLoaded', function() {
-	// Create a RGB raster object for the 2D canvas.
-	// JSARToolKit uses raster objects to read image data.
-	// Note that you need to set canvas.changed = true on every frame.
-	var raster = new NyARRgbRaster_Canvas2D(canvas);
-
-	// FLARParam is the thing used by FLARToolKit to set camera parameters.
-	// Here we create a FLARParam for images with 320x240 pixel dimensions.
-	var param = new FLARParam(320, 240);
-
-	// The FLARMultiIdMarkerDetector is the actual detection engine for marker detection.
-	// It detects multiple ID markers. ID markers are special markers that encode a number.
-	var detector = new FLARMultiIdMarkerDetector(param, 120);
-
-	// For tracking video set continue mode to true. In continue mode, the detector
-	// tracks markers across multiple frames.
-	detector.setContinueMode(true);
-
-	// Copy the camera perspective matrix from the FLARParam to the WebGL library camera matrix.
-	// The second and third parameters determine the zNear and zFar planes for the perspective matrix.
-	param.copyCameraMatrix(display.camera.perspectiveMatrix, 10, 10000);
-	
-	
-	
-	var video = document.createElement('video');
-	video.width = 640;
-	video.height = 480;
-
-	var getUserMedia = function(t, onsuccess, onerror) {
-	  if (navigator.getUserMedia) {
-		return navigator.getUserMedia(t, onsuccess, onerror);
-	  } else if (navigator.webkitGetUserMedia) {
-		return navigator.webkitGetUserMedia(t, onsuccess, onerror);
-	  } else if (navigator.mozGetUserMedia) {
-		return navigator.mozGetUserMedia(t, onsuccess, onerror);
-	  } else if (navigator.msGetUserMedia) {
-		return navigator.msGetUserMedia(t, onsuccess, onerror);
-	  } else {
-		onerror(new Error("No getUserMedia implementation found."));
-	  }
-	};
-
-	var URL = window.URL || window.webkitURL;
-	var createObjectURL = URL.createObjectURL || webkitURL.createObjectURL;
-	if (!createObjectURL) {
-	  throw new Error("URL.createObjectURL not found.");
-	}
-
-	getUserMedia({'video': true},
-	  function(stream) {
-		var url = createObjectURL(stream);
-		video.src = url;
-	  },
-	  function(error) {
-		alert("Couldn't access webcam.");
-	  }
-	);
-
-
-	
-	// Draw the video frame to the raster canvas, scaled to 320x240.
-	canvas.getContext('2d').drawImage(video, 0, 0, 640, 480);
-
-	// Tell the raster object that the underlying canvas has changed.
-	canvas.changed = true;
-
-	// Do marker detection by using the detector object on the raster object.
-	// The threshold parameter determines the threshold value
-	// for turning the video frame into a 1-bit black-and-white image.
-	//
-	var markerCount = detector.detectMarkerLite(raster, threshold);
-	
-	
-	
-	
-	// Create a NyARTransMatResult object for getting the marker translation matrices.
-	var resultMat = new NyARTransMatResult();
-
-	var markers = {};
-
-	// Go through the detected markers and get their IDs and transformation matrices.
-	for (var idx = 0; idx < markerCount; idx++) {
-	  // Get the ID marker data for the current marker.
-	  // ID markers are special kind of markers that encode a number.
-	  // The bytes for the number are in the ID marker data.
-	  var id = detector.getIdMarkerData(idx);
-
-	  // Read bytes from the id packet.
-	  var currId = -1;
-	  // This code handles only 32-bit numbers or shorter.
-	  if (id.packetLength <= 4) {
-		currId = 0;
-		for (var i = 0; i < id.packetLength; i++) {
-		  currId = (currId << 8) | id.getPacketData(i);
+﻿(function() {	//Not sure what this is supposed to do, but without it don't function so swell
+	//When this document loads
+	window.addEventListener('DOMContentLoaded', function() {
+		var vid = document.getElementById('vid');	//our video
+		var datCanvass = document.getElementById('datCanvass');	//our canvas element (where the magic tends to happen
+		var ctx = datCanvass.getContext('2d');	//more canvas stuff, stuff that processes things to be drawn
+		var w = 640;	//video resolution stuff
+		var h = 480;	//ditto
+		
+		var constraints = {	//variable to alter when i figure out how to access specific cameras for mobile devices
+			video: true,	//yes beedeyo
+			audio:false	//no audio
+		};
+		function successCallback(stream) {	//When the media-call is a WIN
+			var url = window.URL || window.webkitURL;	//I have no idea what this does, but it is necessary for streaming from cam.
+			vid.src = url ? url.createObjectURL(stream) : stream;	//Set the video's source(the video). Not sure about this specific code in it though. It's foreign to source linking that I'm used to, but necessary for streaming from cam.
+			// Set the video to play
+			vid.play();
 		}
-	  }
+		function errorCallback(error) {	//When the media call is a LOSE
+			alert("Could not access your camera for some reason.");
+			return;
+		}
 
-	  // If this is a new id, let's start tracking it.
-	  if (markers[currId] == null) {
-		markers[currId] = {};
-	  }
-	  // Get the transformation matrix for the detected marker.
-	  detector.getTransformMatrix(idx, resultMat);
+		//For allowing the getUserMedia call to be a cross-browser experience
+		navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+		if (navigator.getUserMedia) {
+			navigator.getUserMedia(constraints, successCallback, errorCallback);	//The magic call for accessing getUserMedia. Counts as a cross-domain call and as such does not work so-good on LOCAL.
+		}
+		else {
+			alert('Sorry, the browser you use is inferior. Update and assimilate');	//If you have a camera but your browser is not up to my personal standards of being able to handle me
+			return;
+		}
 
-	  // Copy the result matrix into our marker tracker object.
-	  markers[currId].transform = Object.asCopy(resultMat);
-	}
-
-
-
-	function copyMarkerMatrix(arMat, glMat) {
-	  glMat[0] = arMat.m00;
-	  glMat[1] = -arMat.m10;
-	  glMat[2] = arMat.m20;
-	  glMat[3] = 0;
-	  glMat[4] = arMat.m01;
-	  glMat[5] = -arMat.m11;
-	  glMat[6] = arMat.m21;
-	  glMat[7] = 0;
-	  glMat[8] = -arMat.m02;
-	  glMat[9] = arMat.m12;
-	  glMat[10] = -arMat.m22;
-	  glMat[11] = 0;
-	  glMat[12] = arMat.m03;
-	  glMat[13] = -arMat.m13;
-	  glMat[14] = arMat.m23;
-	  glMat[15] = 1;
-	}
-});
+		// When the video plays(should be on auto in markup, 	TODO: if not, fix later)
+		vid.addEventListener('play', function() {
+			// Every 24 milliseconds copy the video image to the canvas
+			setInterval(function() {
+				ctx.fillRect(0, 0, w, h);	//DRAW A RECTANGLE ON CANVAS TO BE FANCY IF THE VIDEO ISNT THERE
+				ctx.drawImage(vid, 0, 0, w, h);	//DRAW THE VIDEO ON CANVAS TO BE FANCY IF THE VIDEO IS THERE
+			}, 24);
+		}, false);	//FALSE, like everything we thought we believed.
+		
+	})
+})();
