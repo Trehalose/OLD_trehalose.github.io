f4dd5fe238831833514c73e681918f2d9dd8efc1
Trehalose <crisisabort@yahoo.com>
2015-08-06 23:05:35 -0400

    Deleting a lot
    Deleting all unused files or projects

---
+++ kanjiclash/js/engine.js
@@ -0,0 +1,33 @@
+function Character(){
+	this.name = name;
+	this.lvl = lvl;
+	this.hp = hp;
+	this.str = str;
+	this.dex = dex;
+	this.con = con;
+	this.int = int;
+	this.wis = wis;
+	this.cha = cha;
+	this.atk[] = atk[];
+}
+function Player(){
+	this.name = name;
+	this.avatar = avatar;
+	this.hp = hp;
+	this.lvl = lvl;
+	this.str = str;
+	this.dex = dex;
+	this.con = con;
+	this.int = int;
+	this.wis = wis;
+	this.cha = cha;
+	this.atk[] = atk[];
+}
+function Attack(){
+	this.name = name;
+	this.dmg = dmg;
+	this.percentage = percentage;
+	this.calcHit = function(){
+			
+	};
+}
